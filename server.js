8e0177c6c9eed019647c44d60e54a50dfb9c09f2
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-10-09 02:47:22 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+import mongoose from "mongoose";
 const app = require("./app.js");
+//import app from "./app.js";
+//LKOFtbNWMdrUaJv6
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const DB_HOST = "mongodb+srv://Vasylysa:LKOFtbNWMdrUaJv6@cluster0.xqqnczp.mongodb.net/my-contacts?retryWrites=true&w=majority";
+
+console.log(process.env);
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+
